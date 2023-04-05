f0f81fc85483f6af6a10d77aecdf6abba90f55c7
ersinev <ersinevci@hotmail.com>
2023-04-05 11:15:24 +0200

    intro

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,14 @@ public class Main {
         System.out.print(name);
         int myNumber = 15;
         System.out.println(myNumber) ;
+        final int myConstNumber =23;
+        final String surname ="ev";
+
+
+
+
+
+
 
     }
 }
