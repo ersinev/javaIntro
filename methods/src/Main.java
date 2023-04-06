79a4761f2ae0126f6d82fa113dc4c484fc07b2fb
ersinev <ersinevci@hotmail.com>
2023-04-06 10:29:13 +0200

    methods

---
+++ methods/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        bodyIndex();
+    }
+    public static void bodyIndex(){
+        int weight = 60;
+        double height = 1.8;
+        double Index = weight/(height*height);
+        System.out.println(Index);
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 public class Main {
+
+
     public static void main(String[] args)
 
 
@@ -11,11 +13,30 @@ public class Main {
         final int myConstNumber =23;
         final String surname ="ev";
 
+        String name1 = new String("sefa");
+        String name2 = new String("sefa");
+
+        name1="ali";
+
+        if (name1==name2){
+            System.out.println("ayni");
+
+
+
+
+        }else{
+            System.out.println("aynidegil");
+            System.out.println(name1);
+        }
 
 
 
 
 
+
+
+    }
+    public static void findingNumbers(){
 
     }
 }
