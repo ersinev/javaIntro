9a87b3918121a040b19c9c96535b5f3e5f698a40
ersinev <ersinevci@hotmail.com>
2023-04-08 11:52:35 +0200

    updated

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,29 @@
 public class Main {
 
 
-    public static void main(String[] args)
-
-
-    {
+    public static void main(String[] args) {
         //intro
         String name = "ersin";
         System.out.print(name);
         int myNumber = 15;
-        System.out.println(myNumber) ;
-        final int myConstNumber =23;
-        final String surname ="ev";
+        System.out.println(myNumber);
+        final int myConstNumber = 23;
+        final String surname = "ev";
 
         String name1 = new String("sefa");
         String name2 = new String("sefa");
 
-        name1="ali";
+        name1 = "ali";
 
-        if (name1==name2){
+        if (name1 == name2) {
             System.out.println("ayni");
 
 
-
-
-        }else{
+        } else {
             System.out.println("aynidegil");
             System.out.println(name1);
         }
 
-
-
-
-
-
-
-    }
-    public static void findingNumbers(){
 
     }
 }
