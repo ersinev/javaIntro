9db61f77ff71cf8acbc7e943d39821196453dbfc
ersinev <ersinevci@hotmail.com>
2023-04-09 03:18:51 +0200

    updated

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,11 @@ public class Main {
         String name = "ersin";
         System.out.print(name);
         int myNumber = 15;
+
         System.out.println(myNumber);
         final int myConstNumber = 23;
         final String surname = "ev";
+        final String name4 = "ev";
 
 
 
